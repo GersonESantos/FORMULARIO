bdc0ad3b9552c00869fc165b3257b17ab5ac95ca
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-10 21:13:50 -0300

    a

--- FORMULARIO/config.php
+++ FORMULARIO/config.php
@@ -1,5 +1,16 @@
 <?php
     $dbHost = 'Localhost';
-    $dbUsername = 'root'
-    $dbPassword = ''
+    $dbUsername = 'root';
+    $dbPassword = ''; 
+    $dbname = 'cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbname);
+    if($conexao->connect_errno){
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexao com sucesso";
+    }
+
 ?>
