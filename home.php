981e358ddf0bc0b173e9c43ab39d3420736678c5
Gerson Santos <gersones@outlook.com>
2023-01-12 20:49:01 -0300

    aa

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+        font-family: Arial, Helvetica, sans-serif; 
+        
+        }
+    </style>
+</head>
+<body>
+    <h1>Inscreva</h1>
+    <h2>GersonES</h2>
+    <div class="box">
+        <a href="">Login</a>
+        <a href="">Cadastre</a>
+    </div>
+    
+</body>
+</html>
