e5d5d230e1e49587ae0d1c48d5509d964974bf93
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-12 20:59:26 -0300

    Update home.php

--- home.php
+++ home.php
@@ -8,16 +8,35 @@
     <style>
         body{
         font-family: Arial, Helvetica, sans-serif; 
-        
+        background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        text-align: center;
+        color: white; 
         }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 30px;
+            border-radius: 15px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            border: 2px solid dodgerblue;
+            border-radius: 15px;
+            padding: 10px;
+        }
+        a:hover{background-color: dodgerblue;}
     </style>
 </head>
 <body>
-    <h1>Inscreva</h1>
+    <h1>Inscreva-se</h1>
     <h2>GersonES</h2>
     <div class="box">
         <a href="">Login</a>
-        <a href="">Cadastre</a>
+        <a href="formulario.php">Cadastre</a>
     </div>
     
 </body>
