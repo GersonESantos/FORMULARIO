0d50116544d368e4368fcb66b222c771ecfcc718
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-13 20:03:02 -0300

    a

--- sistema.php
+++ sistema.php
@@ -1,5 +1,13 @@
 <?php 
+    session_start();
     print_r($_SESSION);
+    if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true))
+        {
+            unset($_SESSION['email']);
+            unset($_SESSION['senha']);
+            header('location: login.php');
+    }
+    $logado = $_SESSION['email'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
