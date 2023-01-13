6faa83dfd80652b88c617545a784e505ea6365a9
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-13 17:44:46 -0300

    a

--- testLogin.php
+++ testLogin.php
@@ -5,15 +5,28 @@
         include_once('config.php');
         $email = $_POST['email'];
         $senha = $_POST['senha'];
+       // print_r('<br>');
        // print_r('Email: ' . $email);
+       // print_r('<br>');
        // print_r('Senha: ' . $senha);
-       $sql = "SELECT * FROM usuarios";
+       $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha' ";
        $result = $conexao->query($sql);
-       print_r($sql);
-       print_r($result);
-    }
-    else
-    {
+       //print_r('<br>');
+      // print_r($sql);
+      // print_r('<br>');
+      // print_r($result);
+       //print_r('<br>');
+       if(mysqli_num_rows($result) < 1)
+       {
+        //print_r('Nao Existe');
         header('Location: login.php');
+       }
+       else
+       {
+        //print_r('Existe');
+        header('Location: sistema.php');
+       }
+       
     }
+   
     ?>
