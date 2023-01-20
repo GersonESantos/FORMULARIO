28223b2ae0ce408fb80978181e9c8bbf437eb6c7
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-19 22:00:04 -0300

    a

--- login.php
+++ login.php
@@ -8,18 +8,23 @@
     <style>
     body{
         font-family: Arial, Helvetica, sans-serif;
-        background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-
+        background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
         }
     div{
-        background-color: rgba 0, 0, 0, 0, 0.8;
-        position:  absolute;
+        background-color: rgba(0, 0, 0, 0.6);
+        position: absolute;
         top: 50%;
         left: 50%; 
-        transform: translate(-50%, -50%);
+        transform: translate(-50%,-50%);
         padding: 80px;
         border-radius: 15px;
-        color: white;
+        color: #fff;
+         }
+         input{
+        padding: 15px;
+        border: none;
+        outline: none;
+        font-size: 15px;
     }
     .inputSubmit{
         background-color: dodgerblue;
@@ -28,18 +33,11 @@
         width: 100%;
         border-radius: 10px;
         color: white;
-    }
-    input{
-        padding: 15px;
-        border: none;
-        outline: none;
-        font-size: 15px;
-    }
-     
+        font-size: 15px; 
+    }  
     .inputSubmit:hover{
         background-color: deepskyblue;
         cursor: pointer;
-
     }
     </style>
 </head>
@@ -48,9 +46,9 @@
     <div>
         <h1>Login</h1>
         <form action="home.php" method="POST">
-            <input type="email" name="Email" placeholder="Email">
+            <input type="email" name="email" placeholder="Email">
             <br><br>
-            <input type="password" name="Senha" placeholder="Senha">
+            <input type="password" name="senha" placeholder="Senha">
             <br><br>
             <input class="inputSubmit" type="submit" name="submit" value="Enviar">
         </form>
